6f8dca6ee2d710ad1ae1ddff4bade92fc51e4337
Fernando Hirsch <144632964+ochocss@users.noreply.github.com>
2024-09-05 12:31:40 -0300

    Update Main.java

--- src/number/Main.java
+++ src/number/Main.java
@@ -1,5 +1,6 @@
 package number;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -12,10 +13,27 @@ public class Main {
 		System.out.println("Quantidade de dígitos: " + NumberUtils.countDigits(num));
 		System.out.println("Invertido: " + NumberUtils.reverse(num));
 		System.out.println("Soma dos dígitos: " + NumberUtils.sumDigits(num));		
-		System.out.println("Divisível por: " + NumberUtils.getDiv(num));
+		System.out.println("Divisível por: " + getDiv(num));
 		System.out.println("Capícua: " + NumberUtils.checkCapicua(num));
 		System.out.println("Primo: " + NumberUtils.checkPrime(num));
 		
 		sc.close();
+	}
+	
+	public static ArrayList<Byte> getDiv(long num) {
+		ArrayList<Byte> divArray = new ArrayList<>();
+		divArray.add((byte) 1);
+		
+		if(NumberUtils.divBy2(num)) { divArray.add((byte) 2); }
+		if(NumberUtils.divBy3(num)) { divArray.add((byte) 3); }
+		if(NumberUtils.divBy4(num)) { divArray.add((byte) 4); }
+		if(NumberUtils.divBy5(num)) { divArray.add((byte) 5); }
+		if(NumberUtils.divBy6(num)) { divArray.add((byte) 6); }
+		if(NumberUtils.divBy7(num)) { divArray.add((byte) 7); }
+		if(NumberUtils.divBy8(num)) { divArray.add((byte) 8); }
+		if(NumberUtils.divBy9(num)) { divArray.add((byte) 9); }
+		if(NumberUtils.divBy10(num)) { divArray.add((byte) 10); }
+		
+		return divArray;
 	}
 }
